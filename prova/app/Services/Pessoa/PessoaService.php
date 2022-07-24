bbe9a0883513aca218dbf46e89fc73b93ce18143
Wagner Ramos Limas <wagnerramosl@yahoo.com.br>
2022-07-24 16:18:35 -0300

    Implementacao de vinculacao no perfil

--- prova/app/Services/Pessoa/PessoaService.php
+++ prova/app/Services/Pessoa/PessoaService.php
@@ -3,6 +3,8 @@
 namespace App\Services\Pessoa;
 
 use App\Models\Pessoa;
+use App\Services\Experiencia\ExperienciaService;
+use App\Services\Formacao\FormacaoService;
 use App\Services\Pessoa\Interfaces\IPessoaService;
 use Exception;
 use Illuminate\Support\Facades\DB;
@@ -14,12 +16,31 @@ class PessoaService implements IPessoaService
     {
         DB::beginTransaction();
         try {
-           $perfil = Pessoa::create($request);
+            $perfil = Pessoa::create($request);
+
+            $formacoes = $this->mapearIdPerfil($perfil->id, $request['formacao']);
+            $formacao = (new FormacaoService());
+            $formacao->vincularFormacao($formacoes);
+
+            $experiencias = $this->mapearIdPerfil($perfil->id, $request['experiencia']);
+            $experiencia = (new ExperienciaService());
+            $experiencia->vincularExperiencia($experiencias);
+
             DB::commit();
             return $perfil;
-        }catch (Exception $errors){
+        } catch (Exception $errors) {
             DB::rollBack();
-            return 'Mensagem: ' .$errors->getMessage();
+            return 'Mensagem: ' . $errors->getMessage();
         }
+    }
+
+    private function mapearIdPerfil($id, $tipo)
+    {
+        $aux = array();
+        foreach ($tipo as $t){
+            $t['perfil_id'] = $id;
+            $aux[] = $t;
+        }
+        return $aux;
     }
 }
