14669fe35241c54afefdb67934d2c7e12b495611
Wagner Ramos Limas <wagnerramosl@yahoo.com.br>
2022-07-24 00:26:51 -0300

    Ajuste campos requisicao

--- prova/app/Http/Requests/PessoaRequest.php
+++ prova/app/Http/Requests/PessoaRequest.php
@@ -14,12 +14,15 @@ class PessoaRequest extends FormRequest
     public function rules()
     {
         return [
-            'cpf' => 'required',
-            'nome' => 'required',
-            'data_nascimento' => 'required',
-            'email' => 'required',
-            'telefone' => 'required',
-            'resumo' => 'required'
+            'cpf' => 'required|int',
+            'nome' => 'required|max:100',
+            'data_nascimento' => 'required|string',
+            'email' => 'required|email',
+            'formacao' => 'required|array',
+            'experiencia' => 'required|array',
+            'competencia' => 'required|array',
+            'telefone' => 'required|max:15',
+            'resumo' => 'required|max:500'
         ];
     }
 }
