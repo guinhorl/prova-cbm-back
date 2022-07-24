7aafb32795935a424091cca2e033c10754db8f79
Wagner Ramos Limas <wagnerramosl@yahoo.com.br>
2022-07-24 12:52:55 -0300

    ajuste function vincular formacao

--- prova/app/Services/Formacao/FormacaoService.php
+++ prova/app/Services/Formacao/FormacaoService.php
@@ -5,18 +5,20 @@ namespace App\Services\Formacao;
 use App\Models\Formacao;
 use App\Services\Formacao\Interfaces\IFormacaoService;
 use Exception;
-use Illuminate\Support\Facades\DB;
 
 class FormacaoService implements IFormacaoService
 {
-    public function vincularFormacao(array $data)
+    public function vincularFormacao(array $formacoes)
     {
-        DB::beginTransaction();
         try {
-            Formacao::create($data);
-            DB::commit();
+            $da_formacao = (new Formacao());
+            foreach ($formacoes as $formacao){
+                $da_formacao->nome = $formacao['nome'];
+                $da_formacao->perfil_id = $formacao['perfil_id'];
+                $da_formacao->instituicao_id = $formacao['instituicao_id'];
+                $da_formacao->save();
+            }
         } catch (Exception $erro) {
-            DB::rollBack();
             return 'Erro no vincularFormacao(): ' . $erro->getMessage();
         }
     }
