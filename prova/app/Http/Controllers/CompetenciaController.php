4a50b5def13ef38e15bb1913f8a8c5debfa6fef0
Wagner Ramos Limas <wagnerramosl@yahoo.com.br>
2022-07-25 09:28:22 -0300

    documentacao competencias

--- prova/app/Http/Controllers/CompetenciaController.php
+++ prova/app/Http/Controllers/CompetenciaController.php
@@ -8,6 +8,36 @@ use Illuminate\Http\Request;
 class CompetenciaController extends Controller
 {
 
+    /**
+     * Lista de Competencias.
+     * @OA\Get (
+     *     path="/api/v0/competencias/",
+     *     tags={"Index"},
+     *     @OA\Response(
+     *         response=200,
+     *         description="success",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 type="array",
+     *                 property="rows",
+     *                 @OA\Items(
+     *                     type="object",
+     *                     @OA\Property(
+     *                         property="id",
+     *                         type="number",
+     *                         example="1"
+     *                     ),
+     *                     @OA\Property(
+     *                         property="nome",
+     *                         type="string",
+     *                         example="palavra"
+     *                     )
+     *                 )
+     *             )
+     *         )
+     *     )
+     * )
+     */
     public function index()
     {
         $competencias = Competencia::all();
