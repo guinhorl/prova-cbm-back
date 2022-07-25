06cfc3c4d98f7a17d5f042aa41549b641b4b487f
Wagner Ramos Limas <wagnerramosl@yahoo.com.br>
2022-07-25 09:33:09 -0300

    documentacao instituicoes

--- prova/app/Http/Controllers/InstituicaoController.php
+++ prova/app/Http/Controllers/InstituicaoController.php
@@ -7,6 +7,36 @@ use Illuminate\Http\Request;
 
 class InstituicaoController extends Controller
 {
+    /**
+     * Lista de Instituicoes.
+     * @OA\Get (
+     *     path="/api/v0/instituicoes/",
+     *     tags={"Index"},
+     *     @OA\Response(
+     *         response=200,
+     *         description="success",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 type="array",
+     *                 property="rows",
+     *                 @OA\Items(
+     *                     type="object",
+     *                     @OA\Property(
+     *                         property="id",
+     *                         type="number",
+     *                         example="1"
+     *                     ),
+     *                     @OA\Property(
+     *                         property="nome",
+     *                         type="string",
+     *                         example="palavra"
+     *                     )
+     *                 )
+     *             )
+     *         )
+     *     )
+     * )
+     */
     public function index()
     {
         $instituicoes = Instituicao::all();
