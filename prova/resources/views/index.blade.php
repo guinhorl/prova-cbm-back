6249b9201b84257f409ae62baa355700973929be
Wagner Ramos Limas <wagnerramosl@yahoo.com.br>
2022-07-25 12:25:01 -0300

    Readme

--- prova/resources/views/index.blade.php
+++ prova/resources/views/index.blade.php
@@ -2,46 +2,7 @@
 @section('conteudo')
     <div class="container-fluid">
         <div class="row">
-            <div class="col">
-                <div class="text-center">
-                    <h4>Candidatos</h4>
-                </div>
-            </div>
-            <br>
-            <br>
-            <div class="row">
-            <div class="col">
-                <table class="table table-striped table-hover table-bordered">
-                    <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">First</th>
-                        <th scope="col">Last</th>
-                        <th scope="col">Handle</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td colspan="2">Larry the Bird</td>
-                        <td>@twitter</td>
-                    </tr>
-                    </tbody>
-                </table>
-            </div>
-            </div>
+
         </div>
     </div>
 @endsection
