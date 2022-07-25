405498611c00a85d50c846013fc55b53c1908a63
Wagner Ramos Limas <wagnerramosl@yahoo.com.br>
2022-07-25 09:45:37 -0300

    documentacao signo

--- prova/app/Http/Controllers/SignoController.php
+++ prova/app/Http/Controllers/SignoController.php
@@ -8,6 +8,36 @@ use Illuminate\Http\Request;
 class SignoController extends Controller
 {
 
+    /**
+     * Lista de Signos.
+     * @OA\Get (
+     *     path="/api/v0/instituicoes/",
+     *     tags={"Index"},
+     *     @OA\Response(
+     *         response=200,
+     *         description="success",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 type="array",
+     *                 property="rows",
+     *                 @OA\Items(
+     *                     type="object",
+     *                     @OA\Property(
+     *                         property="id",
+     *                         type="number",
+     *                         example="1"
+     *                     ),
+     *                     @OA\Property(
+     *                         property="nome",
+     *                         type="string",
+     *                         example="Signo"
+     *                     )
+     *                 )
+     *             )
+     *         )
+     *     )
+     * )
+     */
     public function index()
     {
         $signos = Signo::all();
